1623d2803a537900dc04dd29e62fba7b3c44982a
weipeng.wang <pythonvoid@outlook.com>
2023-02-14 10:16:46 +0800

    add comment

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,14 @@ use wasm_bindgen::prelude::*;
 
 mod wasm_ptr {
   macro_rules! wasm_ptr_transform {
+    /*
+      rust传给js的对象，是一个WasmRefCell, 来自这个包：use wasm_bindgen::__rt::WasmRefCell;
+          pub struct WasmRefCell<T: ?Sized> {
+              borrow: Cell<usize>,
+              value: UnsafeCell<T>,
+          }
+      如果通过对象的指针获取对象那么需要解引用WasmRefCell，不能除暴的用指针 + borrow的offset来获取value，因为编译器会对齐字节。
+    */
     ($ptr: expr) => {
       unsafe {
         use wasm_bindgen::__rt::WasmRefCell;
@@ -11,7 +19,7 @@ mod wasm_ptr {
     };
   }
 
-  // js ptr to rust ptr mut
+
   pub fn transform_mut<T>(addr: *mut T) -> *mut T  {
     wasm_ptr_transform!(addr)
   }
